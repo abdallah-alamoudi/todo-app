8a47178653ba96e221af5afd5a6e057ea2614118
abdullahAlamoudi <abdallahalamoudi20@gmail.com>
2024-10-17 17:55:34 +0300

    refactor to use mongodb instead of using fs to store data

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -1,85 +1,20 @@
-const { v4: uuidv4 } = require("uuid");
-const { writeTodos, readTodos, isDuplicate } = require("../utils/helpers");
+const { ObjectId } = require("mongodb");
+const { getDB } = require("../data/db");
 
-class TodoError extends Error {
-  constructor(message, type) {
-    super(message);
-    this.name = "TodoError";
-    this.type = type;
+class Todo {
+  constructor(title, description, completed) {
+    this._id = new ObjectId();
+    this.title = title;
+    this.description = description;
+    this.completed = completed;
+  }
+  static async findAll() {
+    const todoCollection = getDB().collection("todos");
+    return await todoCollection.find({}).toArray();
+  }
+  static async findByID(id) {
+    const todoCollection = getDB().collection("todos");
+    return await todoCollection.findOne({ _id: ObjectId(id) });
   }
 }
-class TodoModel {
-  static getTodos() {
-    return readTodos();
-  }
-  static getTodo(id) {
-    const todos = this.getTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo)
-      throw new TodoError("no todo found with this id " + id, "Not found");
-    return todo;
-  }
-
-  static deleteTodo(id) {
-    const todos = this.getTodos();
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    if (newTodos.length === todos.length) {
-      throw new TodoError("no todo found with this id " + id, "Not found");
-    }
-    writeTodos(newTodos);
-    return true;
-  }
-  static createTodo({ title, description }) {
-    const todo = {
-      id: uuidv4(),
-      title,
-      description,
-      completed: false,
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-    };
-    const todos = this.getTodos();
-
-    // check if title already exist
-    if (isDuplicate(todo.title, todo.id, todos))
-      throw new TodoError(
-        "a todo with this title already exist",
-        "Duplicate title"
-      );
-    todos.push(todo);
-    writeTodos(todos);
-    return todo;
-  }
-
-  static updateTodo(id, updateObj) {
-    // check if todo exist
-    const todos = this.getTodos();
-    const todoInx = todos.findIndex((todo) => todo.id === id);
-    const todo = todos[todoInx];
-    if (todoInx === -1)
-      throw new TodoError(
-        "No todo with this id " + id + " is found",
-        "Not found"
-      );
-
-    if (isDuplicate(updateObj.title, todo.id, todos)) {
-      throw new TodoError(
-        "a todo with this title already exist",
-        "Duplicate title"
-      );
-    }
-
-    // update the completed field
-    updateObj.completed = updateObj.completed === "on";
-    const newTodo = {
-      ...todo,
-      ...updateObj,
-      updated_at: new Date().toISOString(),
-    };
-    // updating todos
-    todos[todoInx] = newTodo;
-    writeTodos(todos);
-    return newTodo;
-  }
-}
-module.exports = { TodoModel, TodoError };
+module.exports = { Todo };
