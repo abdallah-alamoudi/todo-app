c768b85eefd15d9e09b6111627c93e1278e2c7ff
abdullahAlamoudi <abdallahalamoudi20@gmail.com>
2024-10-18 09:48:14 +0300

    add save and delete methods to todo model

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -2,19 +2,30 @@ const { ObjectId } = require("mongodb");
 const { getDB } = require("../data/db");
 
 class Todo {
-  constructor(title, description, completed) {
-    this._id = new ObjectId();
+  constructor({ title, description, completed = false }) {
     this.title = title;
     this.description = description;
     this.completed = completed;
   }
   static async findAll() {
     const todoCollection = getDB().collection("todos");
-    return await todoCollection.find({}).toArray();
+    const result = await todoCollection.find({}).toArray();
+    return result;
   }
   static async findByID(id) {
     const todoCollection = getDB().collection("todos");
-    return await todoCollection.findOne({ _id: ObjectId(id) });
+    const result = await todoCollection.findOne({ _id: new ObjectId(id) });
+    return result;
+  }
+  async save(todo) {
+    const todoCollection = getDB().collection("todos");
+    const result = await todoCollection.insertOne(todo);
+    return result;
+  }
+  async delete(id) {
+    const todoCollection = getDB().collection("todos");
+    const result = await todoCollection.deleteOne({ _id: new ObjectId(id) });
+    return result;
   }
 }
 module.exports = { Todo };
