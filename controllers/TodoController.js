b0d3b0afbbef8b8c49b04a7e73a6d659b2ca157f
abdullahAlamoudi <abdallahalamoudi20@gmail.com>
2024-10-18 09:53:41 +0300

    refactor controller to use mongodb methods instead of fs json

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,19 +1,22 @@
-const { TodoModel, TodoError } = require("../models/TodoModel");
+const { Todo, TodoError } = require("../models/TodoModel");
 const { asyncHandler } = require("../utils/helpers");
-const listTodos = asyncHandler((req, res, next) => {
-  const todos = TodoModel.getTodos();
+const listTodos = asyncHandler(async (req, res, next) => {
+  const todos = await Todo.findAll();
+  res.send(todos);
   res.render("todos/index", { todos, title: "todos" });
 });
-const getTodo = asyncHandler((req, res, next) => {
-  const todo = TodoModel.getTodo(req.params.id);
-  res.render("todos/showTodo", { title: "todo", todo });
+const getTodo = asyncHandler(async (req, res, next) => {
+  const todo = await Todo.findByID(req.params.id);
+  res.send(todo);
+  // res.render("todos/showTodo", { title: "todo", todo });
 });
 const deletePage = asyncHandler((req, res, next) => {
   const todo = TodoModel.getTodo(req.params.id);
   res.render("todos/deleteTodo", { title: "Delete todo", todo });
 });
 const deleteTodo = asyncHandler((req, res, next) => {
-  TodoModel.deleteTodo(req.params.id);
+  const result = TodoModel.deleteTodo(req.params.id);
+  res.send(result);
   res.redirect("/todos");
 });
 const createForm = asyncHandler((req, res, next) => {
@@ -24,7 +27,8 @@ const createForm = asyncHandler((req, res, next) => {
 });
 const create = asyncHandler((req, res, next) => {
   const { title, description } = req.body;
-  const todo = TodoModel.createTodo({ title, description });
+  const todo = new Todo({ title, description });
+  res.send(todo);
   res.redirect(`/todos`);
 });
 const editForm = asyncHandler((req, res, next) => {
